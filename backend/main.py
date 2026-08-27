c4eff61a50e0869bc2d0aa05210eb4ba43056f97
mottupally nikshitha <mottupallynikshitha@gmail.com>
2026-08-27 13:20:48 +0530

    Fix frontend backend connection

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://document-ocr-1.onrender.com",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
